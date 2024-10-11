5576195effbf4a7f4edb6bfa2eb9ba1fc9eeb42f
assa mohammed <mohammedassad749@gmail.com>
2024-10-11 16:06:12 +0300

    add new changes

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+    return drivers.slice(0,2);
+}
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+returnFirstTwoDrivers(drivers)
+console.log(returnFirstTwoDrivers(drivers));
+
+//Return last 2 drivers
+const returnLastTwoDrivers = function () {
+    return drivers.slice(-2)
+} 
+
+returnLastTwoDrivers(drivers);
+console.log(returnLastTwoDrivers(drivers));
+
+//selecting drivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// console.log(selectingDrivers[0]);
+// console.log(selectingDrivers[1]);
+
+//creating fare multiplier
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(nameArray, returningDrivers) {
+    return returningDrivers(nameArray);
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
